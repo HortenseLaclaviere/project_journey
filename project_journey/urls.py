e0a03ea9ce264e604889b0c005f486be5eb5c3e9
Hortense <hortense.laclaviere@student.adatechschool.fr>
2023-10-07 15:37:19 +0200

    refactor project directories

--- project_journey/urls.py
+++ project_journey/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('', include('game.urls')),
+    path('', include('apps.urls')),
     path('admin/', admin.site.urls),
 ]
 

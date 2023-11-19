a8f7fcb631ecb7f056d3a04d3ec66be07bf571c9
Hortense <hortense.laclaviere@student.adatechschool.fr>
2023-11-19 15:22:38 +0100

    Feat(players): create players model #9

--- apps/players/models.py
+++ apps/players/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from apps.users.models import User
 
-# Create your models here.
+
+class Player(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    progression = models.ForeignKey("Stories", on_delete=models.PROTECT)
+    player_tools = models.ManyToManyField("Tools", blank=True)
+
+    def __str__(self):
+        return self.user_id

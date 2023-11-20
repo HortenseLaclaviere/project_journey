0c31aa657c9017f500e7c63b8174350c8a17f66c
Hortense <hortense.laclaviere@student.adatechschool.fr>
2023-11-20 11:11:08 +0100

    refactor(player): refactor player model

--- apps/players/models.py
+++ apps/players/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 
 class Player(models.Model):
-    user_id = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     progression = models.ForeignKey("stories.Story", on_delete=models.PROTECT)
     player_tools = models.ManyToManyField("tools.Tool", blank=True)
 
     def __str__(self):
-        return self.user_id
+        return self.user

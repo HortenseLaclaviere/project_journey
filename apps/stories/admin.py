4ae07fbad5574aeae1ef552184341cec8314945a
Hortense <hortense.laclaviere@student.adatechschool.fr>
2024-02-09 11:03:30 +0100

    feat(story): add class StoryAdmin #14

--- apps/stories/admin.py
+++ apps/stories/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from apps.stories.models import Story
+
 
 # Register your models here.
+class StoryAdmin(admin.ModelAdmin):
+    model = Story
+    list_display = ("pk", "text")
+
+
+admin.site.register(Story, StoryAdmin)

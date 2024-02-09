a7e50da215c78d673d05b3d5b2b7b46b2775c2c1
Hortense <hortense.laclaviere@student.adatechschool.fr>
2024-02-09 11:09:13 +0100

    feat(choices): add class ChoiceAdmin #14

--- apps/choices/admin.py
+++ apps/choices/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from apps.choices.models import Choice
+
+
 # Register your models here.
+class ChoiceAdmin(admin.ModelAdmin):
+    model = Choice
+    list_display = ("pk", "text")
+
+
+admin.site.register(Choice, ChoiceAdmin)

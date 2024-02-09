64c5e6b524d8853338d63915659c5cc92d8da163
Hortense <hortense.laclaviere@student.adatechschool.fr>
2024-02-09 11:20:17 +0100

    feat(players): add class PlayerAdmin #14

--- apps/players/admin.py
+++ apps/players/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+
+from apps.players.models import Player
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    model = Player
+    list_display = ("pk", "user")
+
+
+admin.site.register(Player, PlayerAdmin)

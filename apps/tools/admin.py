24efcc33541ef6b7bf4ab6d11e696b4bae7ecf84
Hortense <hortense.laclaviere@student.adatechschool.fr>
2024-02-09 11:16:12 +0100

    feat(tools): add class ToolAdmin #14

--- apps/tools/admin.py
+++ apps/tools/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from apps.tools.models import Tool
+
+
 # Register your models here.
+class ToolAdmin(admin.ModelAdmin):
+    model = Tool
+    list_display = ("pk", "name")
+
+
+admin.site.register(Tool, ToolAdmin)

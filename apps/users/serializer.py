258566fdacb12661207d1b0f377d503e6b61b8e4
Hortense <hortense.laclaviere@student.adatechschool.fr>
2024-04-16 13:51:29 +0200

    feat(users): format date_joined #18

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -1,16 +1,25 @@
 from .models import User
 from rest_framework import serializers
+from django.utils.formats import date_format
 
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     # Un sérialiseur est utilisé pour convertir les objets du modèle en données JSON pour les API.
     password = serializers.CharField(write_only=True)
+    format_date_joined = serializers.SerializerMethodField()
 
     class Meta:
         # La classe Meta permet de configurer le sérialiseur en spécifiant des métadonnées pour le modèle.
         model = User
-        fields = ["url", "username", "email", "is_staff", "password", "date_joined"]
+        fields = [
+            "url",
+            "username",
+            "email",
+            "is_staff",
+            "password",
+            "format_date_joined",
+        ]
 
     def create(self, validated_data):
         # Utilise create_user pour hasher le mot de passe correctement
@@ -27,3 +36,6 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         if password:
             instance.set_password(password)
         return super().update(instance, validated_data)
+
+    def get_format_date_joined(self, obj):
+        return date_format(obj.date_joined, format="DATE_FORMAT", use_l10n=True)
